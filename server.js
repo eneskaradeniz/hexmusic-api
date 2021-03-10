930154de0b454b0622e73af1bc08a0b78533d902
eneskaradeniz <eneskrdnz28@gmail.com>
2021-03-10 03:25:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const GridFsStorage = require('multer-gridfs-storage');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json());
 app.use(cors());
 app.use(compression());
