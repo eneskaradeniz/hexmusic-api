59070c66ed0aace75358e77c70ab4a7a8cc4cd4b
eneskaradeniz <eneskrdnz28@gmail.com>
2021-03-21 02:43:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,9 @@ io.use(socketioJwt.authorize({
 io.on('connection', socket => {
   connect_socket(socket);
 
-  console.log(io.sockets.clients());
+  io.sockets.sockets.map((e) => {
+    console.log('hello:', e.decoded_token._id);
+  });
 
   socket.on('disconnect', () => disconnect_socket(socket));
   socket.on("start_typing", (data) => start_typing(socket, data));
@@ -88,6 +90,7 @@ function connect_socket(socket) {
     console.log(`(${io.sockets.clients().length})`, "CONNECT SOCKETID:USERID: " + socket.id + ":" + user_id);
 
     // BU USERID LI BAŞKA SOCKET VARMI KONTROL ET
+
     const find_sockets = io.sockets.clients().filter(x => x.decoded_token._id === user_id);
     find_sockets.forEach(x => {
       console.log('foreach:', x);
