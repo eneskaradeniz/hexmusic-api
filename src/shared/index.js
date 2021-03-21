2f2fcc221ecd5dc93393f4ecbcebf208c860f693
eneskaradeniz <eneskrdnz28@gmail.com>
2021-03-21 04:27:25 +0300

    Update index.js

--- src/shared/index.js
+++ src/shared/index.js
@@ -7,17 +7,26 @@ class Socket {
     }
 
     static findSocket(user_id) {
-        const find_socket_id = Object.keys(this.socket_io.sockets.sockets).find(x => x.decoded_token._id === user_id);
-        return this.socket_io.sockets.sockets[find_socket_id];
+        var find_socket;
+
+        for(let x in this.socket_io.sockets.sockets) {
+            const socket = this.socket_io.sockets.sockets[x];
+            if(socket.decoded_token._id === user_id) {
+                find_socket = socket;
+                break;
+            }
+        }
+
+        return find_socket;
     }
 
     static findSockets(user_id) {
         var find_sockets = [];
 
-        Object.keys(this.socket_io.sockets.sockets).forEach((x) => {
-            const find_socket = this.socket_io.sockets.sockets[x];
-            if(find_socket.decoded_token._id === user_id) find_sockets.push(find_socket);           
-        });
+        for(let x in this.socket_io.sockets.sockets) {
+            const socket = this.socket_io.sockets.sockets[x];
+            if(socket.decoded_token._id === user_id) find_sockets.push(find_socket);           
+        }
 
         return find_sockets;
     }
