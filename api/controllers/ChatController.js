961d21676d5fce2495de494183dbecd8a5fb7a80
eneskaradeniz <eneskrdnz28@gmail.com>
2021-04-06 22:43:49 +0300

    Update ChatController.js

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -83,7 +83,7 @@ class ChatController {
 
             return res.status(200).json({
                 success: true,
-                messages: messageBucket != null ? messageBucket.messages : []
+                messages: messageBucket.length > 0 ? messageBucket[0].messages : []
             });
 
         } catch(err) {
